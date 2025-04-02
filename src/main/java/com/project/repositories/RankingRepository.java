f6eb684b3f613c46070baa00204a1b87ce66efb8
David <148752246+kadave462@users.noreply.github.com>
2025-04-02 14:59:02 +0200

    ¨done¨

--- src/main/java/com/project/repositories/RankingRepository.java
+++ src/main/java/com/project/repositories/RankingRepository.java
@@ -1,14 +1,20 @@
 package com.project.repositories;
 
 import com.project.entities.RankingRow;
+import io.micrometer.common.lang.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RankingRepository extends JpaRepository<RankingRow, UUID> {
     List<RankingRow> findAllByOrderByPointsDescGoalDifferenceDescGoalForCountDesc();
     List<RankingRow> findAllByOrderByRankAsc();
+    @EntityGraph(attributePaths = {"team"})
+    @NonNull
+    Optional<RankingRow> findById(@NonNull UUID teamId);
 }
